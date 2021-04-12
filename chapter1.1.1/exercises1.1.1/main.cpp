8a16a7900c79530aa35e43e4766dd87f80ec0581
John0207 <john.deluccia@snhu.edu>
2021-04-12 15:20:13 -0400

    first exercise section

---
+++ chapter1.1.1/exercises1.1.1/main.cpp
@@ -0,0 +1,17 @@
+// compile and run main from page two
+
+#include <iostream>
+
+using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World" << endl;
+//     return 0;
+// }
+
+// change value to return negative 1
+int main()
+{
+    return -1;
+}
--- chapter1.1.1/hello_world/hello_world.cpp
+++ chapter1.1.1/hello_world/hello_world.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// hello world for testing development environment
+
 using namespace std;
 
 int main()
